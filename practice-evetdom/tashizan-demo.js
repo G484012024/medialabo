be0ab2b9304171b926b722a7a0561b654e83d2e6
G484012024 <248401@st.takushoku-u.ac.jp>
2025-05-15 16:40:35 +0900

    提出

---
+++ practice-evetdom/tashizan-demo.js
@@ -0,0 +1,5 @@
+let i=document.querySelector('input[name="left"]');
+let f=document.querySelector('input[name="right"]');
+k=document.querySelector('button#clck');
+k.addEventListener
+
--- practice-js/hello.js
+++ practice-js/hello.js
@@ -4,3 +4,9 @@ x=3;
 x=x+1;
 
 console.log(x);
+//undefined は「未定義」を意味する特別な値です．
+//また，null は「空っぽ」を意味する特別な値です．
+
+
+
+//console.media.room[0]  ps室1を出力
